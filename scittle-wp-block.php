b4048e3ed8618866da969202c6ecc9ecd35dc388
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-05-03 21:25:39 +0300

    fix plugin boilerplate so plugin loads

---
+++ scittle-wp-block.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Scittle WP Block
+Description: Demo block made with ClojureScript
+Version: 0.1
+Author: Jarkko Saltiola
+Author URI: https://codeberg.org/jasalt
+*/
+
+$projectRootDir = __DIR__ . '/';
+require $projectRootDir . 'blocks/scittle-test/scittle-test.php';
